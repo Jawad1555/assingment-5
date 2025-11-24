22a0a23e7eb922483e5c386eb506e65b564f4625
Jawad <jdjawad05@gmail.com>
2025-11-24 20:22:05 +0600

    heart click and count add

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('wee won')
+let count =0;
+const hearts =document.querySelectorAll(".heart-click")
+const heartsCount =document.getElementById('heart-count')
+hearts.forEach(function (heart) {
+    heart.addEventListener("click" , function(){
+        count++; 
+        heartsCount.innerText =count
+
+    })
+})
+
+
