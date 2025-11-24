7a9e9e8831587a66fa4dbc48d8c8e8908c8c5521
Jawad <jdjawad05@gmail.com>
2025-11-24 21:05:24 +0600

    call button add

--- index.js
+++ index.js
@@ -9,4 +9,20 @@ hearts.forEach(function (heart) {
     })
 })
 
+let coin =100;
 
+const callBtn = document.querySelectorAll(".call-btn")
+const totalCoin = document.getElementById("total-coin")
+
+callBtn.forEach(function(btn)){
+    btn.addEventListener("click", 
+        function(){
+            if(coin >= 20){
+                coin =coin -20
+                alert( "you used 20 coin for this call")
+            }
+            else{
+                alert("Not Enough Coin")
+            }
+        })
+}
