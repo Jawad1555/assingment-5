809117cedc7e243d47f139c9a46df8b50aa3f2a2
Jawad <jdjawad05@gmail.com>
2025-11-28 20:17:43 +0600

    responsive

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-let count =0;
-const hearts =document.querySelectorAll(".heart-click")
-const heartsCount =document.getElementById('heart-count')
-hearts.forEach(function (heart) {
-    heart.addEventListener("click" , function(){
-        count++; 
-        heartsCount.innerText =count
+let count = 0;
+const hearts = document.querySelectorAll(".heart-click");
+const heartsCount = document.getElementById("heart-count");
 
-    })
+hearts.forEach(function (heart) {
+    heart.addEventListener("click", function () {
+        count++;
+        heartsCount.innerText = count;
+    });
+});
+
+let copy = 0
+const copyBtn = document.querySelectorAll(".copy-btn")
+const copyCount = document.getElementById("copy-count")
+
+
+copyBtn.forEach(function (ja) {
+    ja.addEventListener("click",
+        function () {
+            copy++
+            copyCount.innerText = copy
+        }
+    )
 })
 
-let coin =100;
+
+
+let coin = 100;
 
 const callBtn = document.querySelectorAll(".call-btn")
 const totalCoin = document.getElementById("total-coin")
 
-callBtn.forEach(function(btn)){
-    btn.addEventListener("click", 
-        function(){
-            if(coin >= 20){
-                coin =coin -20
-                alert( "you used 20 coin for this call")
+callBtn.forEach(function (btn) {
+    btn.addEventListener("click",
+        function () {
+            if (coin >= 20) {
+                coin = coin - 20
+                alert("you used 20 coin for this call")
+                document.getElementById("total-coin").innerText = coin
             }
-            else{
+            else {
                 alert("Not Enough Coin")
             }
         })
-}
+})
